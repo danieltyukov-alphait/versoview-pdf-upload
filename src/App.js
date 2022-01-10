1246a3f8ce14309c960aafd260717516e46df8a1
Daniel <daniel.tyukov.alphait.us>
2022-01-10 23:45:04 +0200

    some fixes and addition of view sdk client

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import FullWindow from "./samples/Embed Modes/FullWindow";
-import PDFAnnotationTools from "./samples/PDFAnnotationTools";
 import SizedContainer from "./samples/Embed Modes/SizedContainer";
 import InLine from "./samples/Embed Modes/InLine";
 import Lightbox from "./samples/Embed Modes/Lightbox";
-import ViewerCustomization from "./samples/ViewerCustomization";
-import CaptureViewSDKEvents from "./samples/CaptureViewSDKEvents";
 import LocalPDFFilePreview from "./samples/LocalPDFFilePreview";
-import CaptureEvents from "./samples/PDF Annotation APIs/CaptureEvents";
-import CRUDAPIs from "./samples/PDF Annotation APIs/CRUDAPIs";
-import HandlingAnnotationsInPDF from "./samples/PDF Annotation APIs/HandlingAnnotationsInPDF";
-import UIConfigurations from "./samples/PDF Annotation APIs/UIConfigurations/UIConfigurations";
 import "./App.css";
+
+// import CaptureEvents from "./samples/PDF Annotation APIs/CaptureEvents";
+// import CRUDAPIs from "./samples/PDF Annotation APIs/CRUDAPIs";
+// import HandlingAnnotationsInPDF from "./samples/PDF Annotation APIs/HandlingAnnotationsInPDF";
+// import UIConfigurations from "./samples/PDF Annotation APIs/UIConfigurations/UIConfigurations";
+
+// import ViewerCustomization from "./samples/ViewerCustomization";
+// import CaptureViewSDKEvents from "./samples/CaptureViewSDKEvents";
+// import PDFAnnotationTools from "./samples/PDFAnnotationTools";
 
 export default class App extends Component {
     render() {
@@ -23,10 +25,12 @@ export default class App extends Component {
                     <Route path="/sizedContainer" component={SizedContainer} />
                     <Route path="/inLine" component={InLine} />
                     <Route path="/lightbox" component={Lightbox} />
-                    <Route path="/pdfAnnotationTools" component={PDFAnnotationTools} />
-                    <Route path="/viewerCustomization" component={ViewerCustomization} />
-                    <Route path="/captureEvents" component={CaptureViewSDKEvents} />
                     <Route path="/localPDFFilePreview" component={LocalPDFFilePreview} />
+
+                    {/* <Route path="/pdfAnnotationTools" component={PDFAnnotationTools} />
+                    <Route path="/viewerCustomization" component={ViewerCustomization} />
+                    <Route path="/captureEvents" component={CaptureViewSDKEvents} /> */}
+
                     {/* <Route exact path="/annotationAPIs" component={CRUDAPIs} />
                     <Route path="/annotationAPIs/captureEvents" component={CaptureEvents} />
                     <Route path="/annotationAPIs/handlingInPDF" component={HandlingAnnotationsInPDF} />
